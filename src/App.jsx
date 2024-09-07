98deca57493f1f4bc066f5c8347df7bf76553669
Yevhen Havrysh <39010905+27ugeen@users.noreply.github.com>
2024-09-07 15:04:00 +0300

    component transactionHistory added

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import './index.css';
 
 import userData from './jsonData/userData.json';
 import friends from './jsonData/friends.json';
+import transactions from './jsonData/transactions.json';
 
 import Profile from './components/Profile/Profile';
 import FriendList from './components/FriendList/FrendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 
 const App = () => {
   return (
@@ -18,6 +20,7 @@ const App = () => {
         stats={userData.stats}
       />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
